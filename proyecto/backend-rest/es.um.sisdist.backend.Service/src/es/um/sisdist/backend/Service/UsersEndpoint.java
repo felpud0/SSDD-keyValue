d03f1ff7e02efeb5fc6a1532a4ad28a4450a776e
felpud0 <felipe.sanchezg@um.es>
2023-04-28 20:20:12 +0200

    endpoint de registro movido

--- proyecto/backend-rest/es.um.sisdist.backend.Service/src/es/um/sisdist/backend/Service/UsersEndpoint.java
+++ proyecto/backend-rest/es.um.sisdist.backend.Service/src/es/um/sisdist/backend/Service/UsersEndpoint.java
@@ -1,13 +1,21 @@
 package es.um.sisdist.backend.Service;
 
+import java.util.Optional;
+
+
 import es.um.sisdist.backend.Service.impl.AppLogicImpl;
+import es.um.sisdist.backend.dao.models.User;
 import es.um.sisdist.models.UserDTO;
 import es.um.sisdist.models.UserDTOUtils;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/u")
 public class UsersEndpoint
@@ -20,5 +28,15 @@ public class UsersEndpoint
     public UserDTO getUserInfo(@PathParam("username") String username)
     {
         return UserDTOUtils.toDTO(impl.getUserByEmail(username).orElse(null));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response registerUser(UserDTO uo) 
+    {
+        System.out.println("ID: "+ uo.getId() +"EL CORREO: "+uo.getEmail()+" EL NOMBRE: "+uo.getName()+" LA CONTRASEÑA: "+uo.getPassword()); 
+        User u = impl.register(uo);
+        return Response.status(Status.CREATED).entity(uo).build();
     }
 }
