74b918f802092ad777c0eef59eecbe9e14e58d97
felpud0 <felipe.sanchezg@um.es>
2023-05-17 23:45:58 +0200

    creado rest externo

---
+++ proyecto/backend-restext/restexterno/src/main/java/es/um/sisdist/External.java
@@ -0,0 +1,10 @@
+package main.java.es.um.sisdist;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class External extends Application {
+    
+}
