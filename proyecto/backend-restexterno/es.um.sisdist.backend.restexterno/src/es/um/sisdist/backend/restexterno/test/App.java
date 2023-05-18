77ba01727d2ac65530f81a5f9a738054710348d0
KillerGon01 <82812379+KillerGon01@users.noreply.github.com>
2023-05-18 19:16:54 +0200

    Intento de restExterno

--- proyecto/backend-restexterno/es.um.sisdist.backend.restexterno/src/es/um/sisdist/backend/restexterno/test/App.java
+++ proyecto/backend-restexterno/es.um.sisdist.backend.restexterno/src/es/um/sisdist/backend/restexterno/test/App.java
@@ -3,7 +3,7 @@ package es.um.sisdist.backend.restexterno.test;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/base")
-public class AppBase extends Application{
+@ApplicationPath("/")
+public class App extends Application{
 
 }
--- proyecto/backend-restexterno/es.um.sisdist.backend.restexterno/src/es/um/sisdist/backend/restexterno/test/Test.java
+++ proyecto/backend-restexterno/es.um.sisdist.backend.restexterno/src/es/um/sisdist/backend/restexterno/test/Test.java
@@ -1,18 +1,17 @@
 package es.um.sisdist.backend.restexterno.test;
 
-import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@ApplicationPath("/")
+@Path("/test")
 public class Test {
-	
+
 	@GET
-	@Path("/test")
-	@Produces ( MediaType . TEXT_PLAIN )
-	public String sayPlainTextHello () {
+
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
 		return " TEST ";
 	}
 
